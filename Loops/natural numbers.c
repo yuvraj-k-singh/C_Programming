799abb080dcef4c27eb7409b37d60135017e35e4
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2025-11-21 16:53:57 +0530

    added n natural numb

---
+++ Loops/natural numbers.c
@@ -0,0 +1,17 @@
+/*
+WAP: Print 1 to 100 natural numbers
+Date: 21-aug-2025
+*/
+
+#include<stdio.h>
+
+int main()
+{
+	int i=1;
+	printf("The natural number 1 to 100 is: \n");
+	for(i=1; i<=100; i++){
+		printf("%d\n", i);
+	}
+	return 0;
+}
+
