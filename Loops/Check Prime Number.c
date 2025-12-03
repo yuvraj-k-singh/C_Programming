eed38e67ea193b0245a54ba57f2b527c02a00ef2
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2025-12-03 23:29:36 +0530

    added check prime

--- Loops/Check Prime Number.c
+++ Loops/Check Prime Number.c
@@ -1,6 +1,6 @@
 /*
-WAP: Check Prime Number of given number
-Date: 22-aug-2025
+WAP: Check prime number of given number
+Date: 23-aug-2025
 */
 
 #include<stdio.h>
@@ -8,22 +8,29 @@ Date: 22-aug-2025
 int main()
 {
 	int i=2, n, prime=0;
-	printf("Enter the value of n: ");
+	printf("Enter the value of number: ");
 	scanf("%d", &n);
 	
-	for(i=2;i<n;i++){
-		if(n%i==0){
-			prime=1;
-			break;
+	if(n<=0){
+		printf("The prime number is not possible for given %d number", n);
+		return 0;
+	} else if(n==1){
+	 prime=1;
+	} else {
+		while(i<n){
+			if(n%i==0){
+				prime=1;
+				break;
+			}
+			i++;
 		}
 	}
 	
 	if(prime==1){
-		printf("The given number %d is not prime number", n);
-	} else{
-		printf("The given number %d is prime number", n);
+		printf("The given number %d is not prime", n);
+	} else {
+		printf("The given number %d is prime", n);
 	}
-	
 	return 0;
 }
 
