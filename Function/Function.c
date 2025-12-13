ad953205b61fb4a0a5fb3c3b6a96870d18c53022
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2025-12-13 22:40:06 +0530

    Function

---
+++ Function/Function.c
@@ -0,0 +1,20 @@
+/*
+WAP: Function
+Date: 23-aug-2025
+*/
+
+#include<stdio.h>
+
+void greet();             //function Declaration or protocol
+
+int main()
+{
+	greet();             //function call
+	return 0;
+}
+
+void greet(){            //function define
+	printf("Hey sir! How are you?");
+	return;
+}
+
