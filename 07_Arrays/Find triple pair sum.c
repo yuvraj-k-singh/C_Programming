c9375eba29f8dc2e09b8dfd54e9db0888f86b0ac
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2026-01-18 00:03:16 +0530

    Find triple pair sum

---
+++ 07_Arrays/Find triple pair sum.c
@@ -0,0 +1,35 @@
+/*
+WAP: Find triple pair sum
+Date: 30-sep-2025
+*/
+
+#include<stdio.h>
+
+int main()
+{
+	int arr[10]={2,4,5,6,7,8,5,3,5,3};
+	int pair=0, i, j, k, x, found=0;
+	
+	printf("Enter the value of element: ");
+	scanf("%d", &x);
+	
+	for(i=0;i<10;i++){
+		for(j=i+1;j<10;j++){
+			for(k=j+1;k<10;k++){
+				if(arr[i]+arr[j]+arr[k]==x){
+				found=1;
+				pair++;
+			    printf("(%d,%d,%d) ", arr[i],arr[j],arr[k]);
+			}
+			}
+		}
+	}
+	
+	if(found==1){
+		printf("\nThe total number of pair whose sum is equal to %d is: %d", x, pair);
+	} else{
+		printf("\nThere is no pair of elements whose sum is equal to %d", x);
+	}
+	return 0;
+}
+
